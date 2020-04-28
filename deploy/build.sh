4bf55cb6a49e0c90706c915472b64e2dfe8e0a86
nikitades <nikitades@pm.me>
2020-04-28 15:49:38 +0300

    Fix: removed unused ports & clear cache at every build

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd "$(dirname "$0")" || echo "CD failed!"
 cd ../
+symfony console cache:pool:clear cache.global_clearer
 docker build -t nikitades/toxic-avenger-bot-php-fpm:latest -f php-fpm.dockerfile .
 docker push nikitades/toxic-avenger-bot-php-fpm:latest
 cd deploy || echo "No deploy folder found!"
